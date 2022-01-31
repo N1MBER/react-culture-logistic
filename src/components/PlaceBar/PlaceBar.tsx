4a913f6033d134964de8f373ee59f6c10a69b587
N1MBER <socialfox33@gmail.com>
2022-01-31 13:47:51 +0300

    feat(PlaceBar): fix styles

--- src/components/PlaceBar/PlaceBar.tsx
+++ src/components/PlaceBar/PlaceBar.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/reducers';
 import { Place } from '../../types/place';
 import { cn } from '../../__private__/utils/bem';
 import {
@@ -10,6 +12,7 @@ import {
   SearchParam,
 } from './PlaceBarSearchField/PlaceBarSearchField';
 import { pins } from '../../__mocks__/google.mock';
+import { useWindowDimensions } from '../../hooks/useWindowDimensions/useWindowDimensions';
 
 import './PlaceBar.scss';
 
@@ -31,6 +34,22 @@ export const PlaceBar = (props: Props) => {
   const [searchParam, setSearchParam] = useState<SearchParam>({});
 
   const [viewMode, setViewMode] = useState<ViewMode>('search');
+
+  const currentPlace = useSelector(
+    (store: RootState) => store.place.currentPlace
+  );
+
+  const { width, height } = useWindowDimensions();
+
+  useEffect(() => {
+    if (typeof currentPlace !== 'undefined') {
+      setPlaces(currentPlace);
+      setViewMode('currentPlace');
+    } else {
+      setPlaces([...pins]);
+      setViewMode('search');
+    }
+  }, [currentPlace]);
 
   useEffect(() => {
     if (open) {
@@ -70,8 +89,15 @@ export const PlaceBar = (props: Props) => {
     <div
       className={cnPlaceBar({ open })}
       style={{
-        ['--place-bar-width' as string]: `${window.innerWidth / 2}px`,
-        ['--place-bar-margin' as string]: `-${window.innerWidth / 2}px`,
+        ['--place-bar-width' as string]: `${
+          (width || window.innerWidth) / 2
+        }px`,
+        ['--place-bar-margin' as string]: `-${
+          (width || window.innerWidth) / 2
+        }px`,
+        ['--place-bar-max-height' as string]: `${
+          (height || window.innerHeight) - 60
+        }px`,
       }}
     >
       {visible && (
