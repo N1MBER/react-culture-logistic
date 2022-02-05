a1ea97885ea53cf17f8bc0c53d289deb3edf4627
N1MBER <socialfox33@gmail.com>
2022-02-05 13:05:21 +0300

    feat(authReducer): add new prop of user type

--- src/store/reducers/authReducer.ts
+++ src/store/reducers/authReducer.ts
@@ -1,20 +1,25 @@
 import { Reducer } from 'redux';
 import { StoreActionType } from '.';
 
+type UserType = 'person' | 'admin';
+
 type State = {
   showAuthWindow: boolean;
   isAuthorized: boolean;
+  userType?: UserType;
 };
 
 const initialState: State = {
   showAuthWindow: false,
   isAuthorized: false,
+  userType: undefined,
 };
 
 export const SET_IS_AUTHORIZED = 'SET_IS_AUTHORIZED';
 export const SET_SHOW_AUTH = 'SET_SHOW_AUTH';
+export const SET_USER_TYPE = 'SET_USER_TYPE';
 
-const actionTypes = [SET_IS_AUTHORIZED, SET_SHOW_AUTH] as const;
+const actionTypes = [SET_IS_AUTHORIZED, SET_SHOW_AUTH, SET_USER_TYPE] as const;
 type ActionTypes = typeof actionTypes[number];
 
 export const authReducer: Reducer<State, StoreActionType<ActionTypes>> = (
@@ -27,6 +32,8 @@ export const authReducer: Reducer<State, StoreActionType<ActionTypes>> = (
       return { ...state, isAuthorized: !!data };
     case SET_SHOW_AUTH:
       return { ...state, showAuthWindow: !!data };
+    case SET_USER_TYPE:
+      return { ...state, userType: data as UserType };
     default:
       return state;
   }
@@ -43,5 +50,12 @@ export const setShowAuth = (flag: boolean) => {
   return {
     type: SET_SHOW_AUTH,
     data: flag,
+  };
+};
+
+export const setUserType = (data: UserType) => {
+  return {
+    type: SET_USER_TYPE,
+    data,
   };
 };
